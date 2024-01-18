444e4e85068550569f2c3105af7abf72d96a36bc
YashAryan <Yashsiwach07@gmail.com>
2024-01-18 19:46:34 +0530

    ADD

---
+++ CSE310/02_DataType/Datatype.java
@@ -0,0 +1,12 @@
+public class Datatype {
+    public static void main(String args[]){
+        char a = 'a';
+        int b = 2;
+        double c = 15.50;
+        float d = (float) 15.0000;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
